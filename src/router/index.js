ffbaa32b582b1166a6b7dc19ee10494a8b059140
Matasx <matasx@seznam.cz>
2022-02-07 14:25:11 +0100

    Allow display all viewer pages on first load when public access.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Dashboard from '@/views/Dashboard.vue'
 import store from '@/store'
 import { RUNTIME_SIDEBAR_SET } from '@/store/actions/runtime'
+import { AUTH_CHECK } from '@/store/actions/auth'
 
 Vue.use(Router)
 
@@ -76,6 +77,8 @@ const router = new Router({
   ]
 })
 
+let loaded = false
+
 const mobileThreshold = 1100
 
 const isMobile = function () {
@@ -90,7 +93,12 @@ const navigate = function (next, delay) {
   }
 }
 
-router.beforeEach((to, _from, next) => {
+router.beforeEach(async (to, _from, next) => {
+  if (!loaded) {
+    await store.dispatch('auth/' + AUTH_CHECK)
+    loaded = true
+  }
+
   const delay = store.getters['runtime/sidebar'] && isMobile()
   if (delay) {
     store.commit('runtime/' + RUNTIME_SIDEBAR_SET, false)
