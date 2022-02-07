d38802f3affaebfeb81da83a32d2864fcedfbd07
Matasx <matasx@seznam.cz>
2022-02-07 15:57:49 +0100

    CPU utilization.

--- src/views/admin/Utilization.vue
+++ src/views/admin/Utilization.vue
@@ -1,9 +1,22 @@
 <template>
   <b-container fluid="xl">
     <h1>CPU utilization</h1>
-    <p v-if="data">
-      {{ data }}
+    <p class="lead">
+      Shows utilization for each trader and total. If the utilization is higher than 100%, the performance of the program may be significantly reduced. In this case, consider to start separate instance.<br />
+      Note: Utilization is calculated from the total time spent in a single trader during the cycle. It consists of total CPU usage and API latency
     </p>
+    <!-- TODO: API (reset) -->
+    <!-- TODO: total -->
+    <!-- TODO: respect order -->
+    <div v-if="data">
+      <div v-for="([symbol, v]) in Object.entries(data.traders)" :key="symbol" class="row pt-1">
+        <div class="col-sm-4"><broker-name :info="info(symbol)" navigation /></div>
+        <div class="col-sm-5 pt-1">
+          <b-progress :value="v" :max="60000" :precision="2" variant="info"></b-progress>
+        </div>
+        <div class="col-sm-2">{{ (v / 600).toFixed(2) }} %</div>
+      </div>
+    </div>
     <b-skeleton-wrapper v-else loading>
       <template #loading>
           <b-skeleton width="85%"></b-skeleton>
@@ -15,10 +28,12 @@
 </template>
 
 <script>
+import BrokerName from '@/components/BrokerName.vue'
 import axios from 'axios'
 import { createNamespacedHelpers } from 'vuex'
 import { AUTH_CHECK } from '@/store/actions/auth'
 const auth = createNamespacedHelpers('auth')
+const events = createNamespacedHelpers('events')
 
 export default {
   name: 'Utilization',
@@ -27,6 +42,12 @@ export default {
       data: null,
       intervalId: null
     }
+  },
+  components: {
+    BrokerName
+  },
+  computed: {
+    ...events.mapGetters(['info'])
   },
   methods: {
     ...auth.mapActions({
